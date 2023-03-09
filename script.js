0b8b9b732da306eca8bde6305663e12e8e2a4414
hewocheesecake <94603292+hewocheesecake@users.noreply.github.com>
2023-03-09 08:43:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// Select the elements from the DOM
 const videoContainer = document.getElementById('video-container');
 const frog = document.getElementById('frog');
 const timeWatched = document.getElementById('time-watched');
 
-// Set initial time to zero
 let secondsWatched = 0;
 
-// Update time watched every second
 setInterval(() => {
   secondsWatched++;
   const minutes = Math.floor(secondsWatched / 60);
@@ -14,10 +11,8 @@ setInterval(() => {
   timeWatched.textContent = `Time Watched: ${minutes}:${seconds.toString().padStart(2, '0')}`;
 }, 1000);
 
-// Play the gif when the page loads
 frog.play();
 
-// Add click listener to the video container
 videoContainer.addEventListener('click', () => {
   // Do nothing since the gif is always playing
 });
